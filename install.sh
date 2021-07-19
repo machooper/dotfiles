b12337441a67e916a5111e25fdbd323fa56cf475
Mac <5686378+machooper@users.noreply.github.com>
2021-07-19 08:44:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 eval "$(starship init bash)"
 
 # Install Code Extensions
-code --install-extension apollographql.vscode-apollo
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension formulahendry.auto-close-tag
-code --install-extension aaron-bond.better-comments
-code --install-extension naumovs.color-highlight
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension github.codespaces
-code --install-extension github.copilot
-code --install-extension eamodio.gitlens
-code --install-extension yzhang.markdown-all-in-one
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension pulkitgangwar.nextjs-snippets
-code --install-extension christian-kohler.path-intellisense
-code --install-extension jakewilson.vscode-placeholder-images
-code --install-extension esbenp.prettier-vscode
-code --install-extension enkia.tokyo-night
-code --install-extension visualstudioexptteam.vscodeintellicode
+# code --install-extension apollographql.vscode-apollo
+# code --install-extension formulahendry.auto-rename-tag
+# code --install-extension formulahendry.auto-close-tag
+# code --install-extension aaron-bond.better-comments
+# code --install-extension naumovs.color-highlight
+# code --install-extension dsznajder.es7-react-js-snippets
+# code --install-extension dbaeumer.vscode-eslint
+# code --install-extension github.codespaces
+# code --install-extension github.copilot
+# code --install-extension eamodio.gitlens
+# code --install-extension yzhang.markdown-all-in-one
+# code --install-extension christian-kohler.npm-intellisense
+# code --install-extension pulkitgangwar.nextjs-snippets
+# code --install-extension christian-kohler.path-intellisense
+# code --install-extension jakewilson.vscode-placeholder-images
+# code --install-extension esbenp.prettier-vscode
+# code --install-extension enkia.tokyo-night
+# code --install-extension visualstudioexptteam.vscodeintellicode
